89326d942f5015d0e46e216247f3c7228e2b7824
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-06-16 15:02:59 +0200

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php 
+
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'shareposts');
+
+// App Root
+ define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+ define('URLROOT', 'http://localhost/sharedposts');
+ // Site Name
+ define('SITENAME', 'SharedPosts');
+ // App Version
+ define('APPVERSION', '1.0.0');

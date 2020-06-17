92dd01b3e57acfe732a5c254b73b426f28ea1b2f
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-06-17 13:04:31 +0200

    Custom flash messaging

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,7 +1,7 @@
 <?php
   class Users extends Controller {
     public function __construct(){
-
+      $this->userModel = $this->model('User');
     }
 
     public function register(){
@@ -26,17 +26,22 @@
 
         // Validate Email
         if(empty($data['email'])){
-          $data['email_err'] = 'Please enter email';
+          $data['email_err'] = 'Pleae enter email';
+        } else {
+          // Check email
+          if($this->userModel->findUserByEmail($data['email'])){
+            $data['email_err'] = 'Email is already taken';
+          }
         }
 
         // Validate Name
         if(empty($data['name'])){
-          $data['name_err'] = 'Please enter name';
+          $data['name_err'] = 'Pleae enter name';
         }
 
         // Validate Password
         if(empty($data['password'])){
-          $data['password_err'] = 'Please enter password';
+          $data['password_err'] = 'Pleae enter password';
         } elseif(strlen($data['password']) < 6){
           $data['password_err'] = 'Password must be at least 6 characters';
         }
@@ -53,7 +58,19 @@
         // Make sure errors are empty
         if(empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])){
           // Validated
-          die('SUCCESS');
+          
+          // Hash Password
+          $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+          
+          // Register User
+          if($this->userModel->register($data)){
+            flash('register_success', 'You are registered and can log in');
+            redirect('users/login');
+          } else {
+            die('something went wrong');
+          }
+
+
         } else {
           // Load view with errors
           $this->view('users/register', $data);
@@ -94,7 +111,7 @@
 
         // Validate Email
         if(empty($data['email'])){
-          $data['email_err'] = 'Please enter email';
+          $data['email_err'] = 'Pleae enter email';
         }
 
         // Validate Password

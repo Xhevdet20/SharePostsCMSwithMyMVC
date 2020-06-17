98fcdddf6a1081440da55a9afd500365fa3a1ceb
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-06-17 16:43:54 +0200

    added editing posts

--- app/models/Post.php
+++ app/models/Post.php
@@ -39,6 +39,23 @@
         }
     }
 
+    public function updatePost($data){
+      // Prepared Statement
+      $this->db->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
+      // Bind value
+      $this->db->bind(':id', $data['id']);
+      $this->db->bind(':title', $data['title']);
+      $this->db->bind(':body', $data['body']);
+
+      // Execute
+
+      if($this->db->execute()){
+        return true;
+      } else {
+        return false;
+      }
+  }
+
     public function getPostById($id){
       $this->db->query('SELECT * FROM posts WHERE id = :id');
       $this->db->bind(':id', $id);
@@ -46,6 +63,5 @@
       $row = $this->db->single();
 
       return $row;
-
     }
   }

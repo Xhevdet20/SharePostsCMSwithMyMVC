3b8473fdcc3332ab510f9554fc0b24dac39c2585
xhevdet <xhevdet.kryeziu@kujtesa.com>
2020-06-17 16:03:22 +0200

    added inserting posts

--- app/models/Post.php
+++ app/models/Post.php
@@ -21,4 +21,21 @@
 
       return $results;
     }
+
+    public function addPost($data){
+        // Prepared Statement
+        $this->db->query('INSERT INTO posts (title, user_id, body) VALUES(:title, :user_id, :body)');
+        // Bind value
+        $this->db->bind(':title', $data['title']);
+        $this->db->bind(':user_id', $data['user_id']);
+        $this->db->bind(':body', $data['body']);
+  
+        // Execute
+
+        if($this->db->execute()){
+          return true;
+        } else {
+          return false;
+        }
+    }
   }
